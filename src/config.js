49781cd943e9848c9ced6fc9497c5b76bf8416b5
satyanarayan95 <satyanarayan.patra5495@gmail.com>
2021-09-12 20:04:31 +0530

    completed

--- src/config.js
+++ src/config.js
@@ -2,14 +2,13 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 // Initialize Firebase
-let fbObj = {
-    apiKey: "AIzaSyCNBORCvNqBXXGeY9AaV3S51Rf1_-TOdig",
-    authDomain: "resume-builder-cb251.firebaseapp.com",
-    projectId: "resume-builder-cb251",
-    storageBucket: "resume-builder-cb251.appspot.com",
-    messagingSenderId: "184174211145",
-    appId: "1:184174211145:web:8768335260e0e362dbcf98",
-    measurementId: "G-CQ0NK5G71N"
-}
-firebase.initializeApp(fbObj);
+const firebaseConfig = {
+    apiKey: "AIzaSyCDH2d5fTog5QHdzATj3EacwdobhuIklOE",
+    authDomain: "resume-builder-c4f20.firebaseapp.com",
+    projectId: "resume-builder-c4f20",
+    storageBucket: "resume-builder-c4f20.appspot.com",
+    messagingSenderId: "979526374229",
+    appId: "1:979526374229:web:19f062906615a60f3a95dc"
+  };
+firebase.initializeApp(firebaseConfig);
 export default firebase;
